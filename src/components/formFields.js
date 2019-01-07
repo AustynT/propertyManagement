88ae619a6cb3f5a329307b23c4f01893f635fd53
AustynT <Austyntravis9@gmail.com>
2019-01-06 19:53:40 -0700

    styled form inputs

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -2,11 +2,16 @@ import React, { Component } from "react";
 
 export default class FormInputs extends Component {
   render() {
-    const { className, title } = this.props;
+    const { className, title, input, type, placeholder } = this.props;
     return (
       <div className="form-input">
-        <lable>{title}</lable>
-        <input className={`${className} form-input`} />
+        <lable className="form-input__title">{title}</lable>
+        <input
+          className={`${className} form-input__input`}
+          type={type}
+          {...input}
+          placeholder={placeholder}
+        />
       </div>
     );
   }
